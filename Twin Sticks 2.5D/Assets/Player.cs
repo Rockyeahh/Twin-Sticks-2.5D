15164bfd03ebfcf62c1c07718491eb19710eff3e
Soren's Coding Land <chaz.com@hotmail.co.uk>
2017-10-22 22:57:04 +0100

    Completed Lecture: 269 Using CrossPlatformInputManager
    This was done on 22/10/17

---
+++ Twin Sticks 2.5D/Assets/Player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log("H: " + CrossPlatformInputManager.GetAxis("Horizontal")); // The "H: " + labels the front of what is logged to the console. So I could anything instead of H:.
+        Debug.Log("V: " + CrossPlatformInputManager.GetAxis("Vertical"));
+       // Debug.Log(CrossPlatformInputManager.GetAxis("Mouse Y"));
+       // Debug.Log(CrossPlatformInputManager.GetAxis("Mouse X"));
+       // Debug.Log(CrossPlatformInputManager.GetButtonDown("Fire1"));
+       // Debug.Log(CrossPlatformInputManager.GetButtonDown("Jump"));
+    }
+}
